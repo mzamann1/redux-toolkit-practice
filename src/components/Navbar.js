37fcb3c75c0075b489117e2c2432c190409a8084
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-09-27 02:43:47 +0500

    cartSlice added

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import { CartIcon } from "../icons";
+const Navbar = () => {
+  const { amount } = useSelector((state) => state.cart);
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <h3>Redux Toolkit Practice</h3>
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./components/shared/modal/modalSlice";
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
+});
